a052745b194df02a919730de0848b35dc19ca92a
TheNuclei <abyssal.nuclei@gmail.com>
2025-08-29 02:41:31 -0600

    Update localization_en.lua
    Updates

--- localization_en.lua
+++ localization_en.lua
@@ -9,14 +9,16 @@ BUYPOISONS_HELP_MESSAGE = {
 	"BUYPOISONS Usage:",
 	"/bp <command> or /BUYPOISONS <command>",
 	"Commands:",
-	"/bp wpX Y 	Buy Components for Y Level X Wound Poison",
-	"/bp cpX Y 	Buy Components for Y Level X Crippling Poison",
-	"/bp dpX Y 	Buy Components for Y Level X Deadly Poison",
-	"/bp mpX Y 	Buy Components for Y Level X Mind-numbing Poison",
-	"/bp ipX Y 	Buy Components for Y Level X Instant Poison",
-	"/bp ap1 Y  Buy Components for Y Agitating Poison",
-	"/bp fp Y Buy Y Flash Powder",
-	"e.g. '/bp wp3 10' will buy all components needed for 10 lots of level 3 Wound Poison.",
+	"/bp wound X 	Buy Components for X Amount of Wound Poison",
+	"/bp crip X 	Buy Components for X Amount of Crippling Poison II",
+	"/bp deadly X 	Buy Components for X Amount of Deadly Poison",
+	"/bp mind X 	Buy Components for X Amount of Mind-numbing Poison III",
+	"/bp inst X 	Buy Components for X Amount of Instant Poison VI",
+	"/bp dis X 		Buy Components for X Amount of Dissolvent Poison II",
+	"/bp cor X 		Buy Components for X Amount of Corrosive Poison II",
+	"/bp agi X  	Buy Components for X Amount of Agitating Poison",
+	"/bp fp X Buy X Flash Powder",
+	"e.g. '/bp dis 10' will buy all components needed for 10 lots of level 2 Dissolvent Poison.",
 	"Warning: Using anything but multiples of 5 will not handle Vial Numbers properly",
 	"as they can only be bought in multiples of 5 from stores.",
 	"",
@@ -64,52 +66,24 @@ BUYPOISONS_SHORTKEY_LEADED_VIAL				="leadedvial";
 BUYPOISONS_SHORTKEY_LETHARGY_ROOT			="lethargyroot";
 BUYPOISONS_SHORTKEY_MAIDENS_ANGUISH			="maidensanguish";
 
-
-
-BuyPoisonsItemInfo[1]["name"] 				= "Deadly Poison V";
+BuyPoisonsItemInfo[1]["name"] 				= "Dissolvent Poison II";
 BuyPoisonsItemInfo[2]["name"] 				= "Instant Poison VI";
-BuyPoisonsItemInfo[3]["name"] 				= "Wound Poison IV";
-BuyPoisonsItemInfo[4]["name"] 				= "Mind-numbing Poison III";
-BuyPoisonsItemInfo[5]["name"] 				= "Crippling Poison II";
-BuyPoisonsItemInfo[6]["name"] 				= "Agitating Poison";
-BuyPoisonsItemInfo[7]["name"] 				= "Deadly Poison IV";
-BuyPoisonsItemInfo[8]["name"] 				= "Instant Poison V";
-BuyPoisonsItemInfo[9]["name"] 				= "Wound Poison III";
-BuyPoisonsItemInfo[10]["name"] 				= "Deadly Poison III";
-BuyPoisonsItemInfo[11]["name"] 				= "Instant Poison IV";
-BuyPoisonsItemInfo[12]["name"] 				= "Wound Poison II";
-BuyPoisonsItemInfo[13]["name"] 				= "Deadly Poison II";
-BuyPoisonsItemInfo[14]["name"] 				= "Mind-numbing Poison II";
-BuyPoisonsItemInfo[15]["name"] 				= "Instant Poison III";
-BuyPoisonsItemInfo[16]["name"] 				= "Wound Poison";
-BuyPoisonsItemInfo[17]["name"] 				= "Deadly Poison";
-BuyPoisonsItemInfo[18]["name"] 				= "Instant Poison II";
-BuyPoisonsItemInfo[19]["name"] 				= "Mind-numbing Poison";
-BuyPoisonsItemInfo[20]["name"] 				= "Crippling Poison";
-BuyPoisonsItemInfo[21]["name"] 				= "Instant Poison";
-BuyPoisonsItemInfo[22]["name"] 				= "Flash Powder";
+BuyPoisonsItemInfo[3]["name"] 				= "Corrosive Poison II";
+BuyPoisonsItemInfo[4]["name"] 				= "Deadly Poison V";
+BuyPoisonsItemInfo[5]["name"] 				= "Mind-Numbing Poison III";
+BuyPoisonsItemInfo[6]["name"] 				= "Wound Poison";
+BuyPoisonsItemInfo[7]["name"] 				= "Crippling Poison II";
+BuyPoisonsItemInfo[8]["name"] 				= "Agitating Poison";
+BuyPoisonsItemInfo[9]["name"]				= "Flash Powder";
 
-BuyPoisonsItemInfo[1]["shortkey"] = "dp5";
-BuyPoisonsItemInfo[2]["shortkey"] = "ip6";
-BuyPoisonsItemInfo[3]["shortkey"] = "wp4";
-BuyPoisonsItemInfo[4]["shortkey"] = "mp3";
-BuyPoisonsItemInfo[5]["shortkey"] = "cp2";
-BuyPoisonsItemInfo[6]["shortkey"] = "ap1";
-BuyPoisonsItemInfo[7]["shortkey"] = "dp4";
-BuyPoisonsItemInfo[8]["shortkey"] = "ip5";
-BuyPoisonsItemInfo[9]["shortkey"] = "wp3";
-BuyPoisonsItemInfo[10]["shortkey"] = "dp3";
-BuyPoisonsItemInfo[11]["shortkey"] = "ip4";
-BuyPoisonsItemInfo[12]["shortkey"] = "wp2";
-BuyPoisonsItemInfo[13]["shortkey"] = "dp2";
-BuyPoisonsItemInfo[14]["shortkey"] = "mp2";
-BuyPoisonsItemInfo[15]["shortkey"] = "ip3";
-BuyPoisonsItemInfo[16]["shortkey"] = "wp1";
-BuyPoisonsItemInfo[17]["shortkey"] = "dp1";
-BuyPoisonsItemInfo[18]["shortkey"] = "ip2";
-BuyPoisonsItemInfo[19]["shortkey"] = "mp1";
-BuyPoisonsItemInfo[20]["shortkey"] = "cp1";
-BuyPoisonsItemInfo[21]["shortkey"] = "ip1";
-BuyPoisonsItemInfo[22]["shortkey"] = "fp1";
+BuyPoisonsItemInfo[1]["shortkey"] = "dis";
+BuyPoisonsItemInfo[2]["shortkey"] = "inst";
+BuyPoisonsItemInfo[3]["shortkey"] = "cor";
+BuyPoisonsItemInfo[4]["shortkey"] = "deadly";
+BuyPoisonsItemInfo[5]["shortkey"] = "mind";
+BuyPoisonsItemInfo[6]["shortkey"] = "wound";
+BuyPoisonsItemInfo[7]["shortkey"] = "crip";
+BuyPoisonsItemInfo[8]["shortkey"] = "agi";
+BuyPoisonsItemInfo[9]["shortkey"] = "fp1";
 
 end
